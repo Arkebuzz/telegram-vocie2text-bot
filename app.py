8af77def69583ba0d6f1f32ecd4022d6e5ec7890
Arkebuzz <aykoret@gmail.com>
2022-10-15 14:56:02 +0300

    New commit

--- app.py
+++ app.py
@@ -4,12 +4,6 @@ import shutil
 
 from aiogram import executor
 
-from utils import sound_to_text as s2t
-
-from utils.logger import logger
-from loader import dp
-from bot_commands.bot_commands import set_default_commands
-
 
 async def start(dp):
     logger.info("Bot has been started")
@@ -29,7 +23,13 @@ if __name__ == '__main__':
         os.mkdir('data')
 
         with open('data/data.txt', 'w') as f:
-            f.write(0)
+            f.write('0')
+
+    from utils import sound_to_text as s2t
+
+    from utils.logger import logger
+    from loader import dp
+    from bot_commands.bot_commands import set_default_commands
 
     v2t = mp.Process(target=s2t.main)
     v2t.start()
